ae403aea45e8c7de07027e25a7cd6512f862ee35
Bidisha <officialbidisha1@gmail.com>
2021-12-02 19:58:17 +0530

    Added header

--- src/app/httpcalls.service.ts
+++ src/app/httpcalls.service.ts
@@ -1,25 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpcallsService {
+
+  public headerDict = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  };
+
+  public requestOptions = {
+    headers: new HttpHeaders(this.headerDict),
+  };
   constructor(private https: HttpClient) {}
 
   getCountries(): Observable<any> {
-    return this.https.get('https://api.covid19api.com/countries');
+    return this.https.get('https://api.covid19api.com/countries', this.requestOptions);
   }
   getbyCountries(): Observable<any> {
-    return this.https.get('https://api.covid19api.com/summary');
+    return this.https.get('https://api.covid19api.com/summary', this.requestOptions);
   }
   getChartDataByCountries(slug: any): Observable<any> {
-    return this.https.get('https://api.covid19api.com/total/country/' + slug);
+    return this.https.get('https://api.covid19api.com/total/country/' + slug, this.requestOptions);
   }
   getCountrySpecific(country): Observable<any> {
     return this.https.get(
-      'https://api.covid19api.com/country/' + country + '/status/confirmed'
+      'https://api.covid19api.com/country/' + country + '/status/confirmed', this.requestOptions
     );
   }
 }
